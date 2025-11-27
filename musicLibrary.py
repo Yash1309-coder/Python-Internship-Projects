75457e38cddffb0ec0b1d57dffccbc964b9fe045
Yash Aggarwal <yashaggarwal668@gmail.com>
2025-11-27 12:24:23 +0530

    jarvis files

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "kodak" : "https://www.youtube.com/watch?v=Ecs-foVS74Q&list=RDEcs-foVS74Q&start_radio=1&pp=ygUFa29kYWugBwHSBwkJCwoBhyohjO8%3D",
+    "raat ki rani" : "https://www.youtube.com/watch?v=mGwz92koI6o&list=RDmGwz92koI6o&start_radio=1&pp=ygUMcmFhdCBraSByYW5poAcB",
+    "shutdown" : "https://www.youtube.com/watch?v=eL9LaMBHIwc&list=RDeL9LaMBHIwc&start_radio=1&pp=ygUNc2h1dGRvd24gc29uZ6AHAQ%3D%3D",
+    "swah" : "https://www.youtube.com/watch?v=Ky-QenzQD6U&list=RDKy-QenzQD6U&start_radio=1&pp=ygUEc3dhaKAHAQ%3D%3D",
+    "101" : "https://www.youtube.com/watch?v=IUFPs3j341k&list=RDIUFPs3j341k&start_radio=1&pp=ygUPMTAxIHNlZWRoZSBtYXV0oAcB",
+
+}
