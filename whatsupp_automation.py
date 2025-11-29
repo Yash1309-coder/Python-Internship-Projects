f012587bac7843f88079992dba2a12d97d67834b
Yash Aggarwal <yashaggarwal668@gmail.com>
2025-11-29 19:18:19 +0530

    Update whatsupp_automation.py

--- whatsupp_automation.py
+++ whatsupp_automation.py
@@ -1,2 +1,8 @@
 import pywhatkit as kit
-kit.sendwhatmsg_instantly("+919053318341" , "hello viswajeet"  ) 
+import pyautogui
+import time
+
+kit.sendwhatmsg_instantly("+919053318341", "Hello! Auto message") # pyright: ignore[reportPrivateImportUsage]
+
+time.sleep(5)   # WhatsApp load hone ka time
+pyautogui.press("enter")   # SEND button auto press
